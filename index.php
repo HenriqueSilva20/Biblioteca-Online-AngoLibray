17202f6e39cbc64bd6c9fcfffd01904a62d5a63b
HenriqueSilva20 <Oliveiraitel@gmail.com>
2022-04-08 13:00:32 +0100

    Pegando os arquivos locais do projeto e levar para o repositiorio

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Criando primeiro reposit[orio Github</title>
+</head>
+<body>
+    
+    <h1>Criando primeiro reposit[orio Github</h1>
+
+    <br>
+
+    <h2>Olá sejam todos bem vindos ao meu arquivo index.php do meu repositorio git</h2>
+
+</body>
+</html>
